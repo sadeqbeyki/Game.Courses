096598a15c88d68a4fc391705727abae85e0f7fe
mardibak <mardibaki@gmail.com>
2023-04-04 23:52:58 +0430

    Add project files.

---
+++ Game.VazKhos/frmGame.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Game.VazKhos
+{
+    partial class frmGame
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            gameArea = new Panel();
+            lblScore = new Label();
+            rockB = new PictureBox();
+            rockA = new PictureBox();
+            PlayerA = new PictureBox();
+            gameTimer = new System.Windows.Forms.Timer(components);
+            label1 = new Label();
+            gameArea.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)rockB).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)rockA).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)PlayerA).BeginInit();
+            SuspendLayout();
+            // 
+            // gameArea
+            // 
+            gameArea.BackColor = Color.Transparent;
+            gameArea.Controls.Add(lblScore);
+            gameArea.Controls.Add(rockB);
+            gameArea.Controls.Add(rockA);
+            gameArea.Controls.Add(PlayerA);
+            gameArea.Location = new Point(12, 12);
+            gameArea.Name = "gameArea";
+            gameArea.Size = new Size(760, 540);
+            gameArea.TabIndex = 0;
+            // 
+            // lblScore
+            // 
+            lblScore.AutoSize = true;
+            lblScore.Font = new Font("Dosis", 15.7499981F, FontStyle.Bold, GraphicsUnit.Point);
+            lblScore.Location = new Point(3, 3);
+            lblScore.Name = "lblScore";
+            lblScore.Size = new Size(82, 29);
+            lblScore.TabIndex = 3;
+            lblScore.Text = "Score: 0";
+            // 
+            // rockB
+            // 
+            rockB.Image = Properties.Resources.rockB;
+            rockB.Location = new Point(626, 465);
+            rockB.Name = "rockB";
+            rockB.Size = new Size(100, 70);
+            rockB.SizeMode = PictureBoxSizeMode.StretchImage;
+            rockB.TabIndex = 2;
+            rockB.TabStop = false;
+            rockB.Tag = "obstacle";
+            // 
+            // rockA
+            // 
+            rockA.Image = Properties.Resources.rockA;
+            rockA.Location = new Point(334, 485);
+            rockA.Name = "rockA";
+            rockA.Size = new Size(66, 50);
+            rockA.SizeMode = PictureBoxSizeMode.StretchImage;
+            rockA.TabIndex = 1;
+            rockA.TabStop = false;
+            rockA.Tag = "obstacle";
+            // 
+            // PlayerA
+            // 
+            PlayerA.BackColor = Color.Transparent;
+            PlayerA.Image = Properties.Resources.PlayerA;
+            PlayerA.Location = new Point(0, 472);
+            PlayerA.Name = "PlayerA";
+            PlayerA.Size = new Size(100, 70);
+            PlayerA.SizeMode = PictureBoxSizeMode.StretchImage;
+            PlayerA.TabIndex = 0;
+            PlayerA.TabStop = false;
+            // 
+            // gameTimer
+            // 
+            gameTimer.Enabled = true;
+            gameTimer.Interval = 20;
+            gameTimer.Tick += gameTimer_Tick;
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.ForestGreen;
+            label1.Location = new Point(-2, 550);
+            label1.Name = "label1";
+            label1.Size = new Size(793, 23);
+            label1.TabIndex = 1;
+            // 
+            // frmGame
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(784, 561);
+            Controls.Add(label1);
+            Controls.Add(gameArea);
+            Name = "frmGame";
+            Text = "Vazism";
+            KeyDown += KeyIsDown;
+            KeyUp += KeyIsUp;
+            gameArea.ResumeLayout(false);
+            gameArea.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)rockB).EndInit();
+            ((System.ComponentModel.ISupportInitialize)rockA).EndInit();
+            ((System.ComponentModel.ISupportInitialize)PlayerA).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel gameArea;
+        private PictureBox PlayerA;
+        private System.Windows.Forms.Timer gameTimer;
+        private Label label1;
+        private PictureBox rockA;
+        private PictureBox rockB;
+        private Label lblScore;
+    }
+}
---
+++ Game.VazKhos/frmGame.cs
@@ -0,0 +1,163 @@
+
+
+namespace Game.VazKhos
+{
+    public partial class frmGame : Form
+    {
+        bool left, right;
+        bool up, down;
+
+        bool jump;
+        int gravity = 20;
+        int force; 
+
+        int jumpSpeed;
+
+        int score = 0;
+        int obstacleSpeed = 10;
+        Random rand = new();
+        int position;
+        bool isGameOver = false;
+
+        public frmGame()
+        {
+            InitializeComponent();
+            //trex
+            GameReset();
+            //end trex
+        }
+
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right) { right = true; }
+            if (e.KeyCode == Keys.Left) { left = true; }
+
+            if (e.KeyCode == Keys.Up) { up = true; }
+            if (e.KeyCode == Keys.Down) { down = true; }
+
+            if (!jump)
+            {
+                if (e.KeyCode == Keys.Space)
+                {
+                    jump = true;
+                    force = gravity;
+                }
+            }
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right) { right = false; }
+            if (e.KeyCode == Keys.Left) { left = false; }
+
+            if (e.KeyCode == Keys.Up) { up = false; }
+            if (e.KeyCode == Keys.Down) { down = false; }
+
+            //trex
+            if (e.KeyCode == Keys.R && isGameOver == true)
+            {
+                GameReset();
+            }
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            if (right == true) { PlayerA.Left += 10; }
+            if (left == true) { PlayerA.Left -= 5; }
+
+            if (up == true) { PlayerA.Top -= 10; }
+            if (down == true) { PlayerA.Top += 10; }
+
+            //trex
+            //PlayerA.Top += jumpSpeed;
+            lblScore.Text = "Score: " + score;
+            //end trex
+
+            if (jump)
+            {
+                PlayerA.Top -= force;
+                force -= 1;
+                //trex
+                //jumpSpeed = -12;
+            }
+            //else
+            //{
+            //    jumpSpeed = 12;
+            //}
+
+            //if (jump == true && force < 0)
+            //{
+            //    jump = false;
+            //}
+            //end trex
+
+            //back to line
+            if (PlayerA.Top + PlayerA.Height >= gameArea.Height)
+            {
+                PlayerA.Top = gameArea.Height - PlayerA.Height;
+                jump = false;
+                //trex
+                //jumpSpeed = 0;
+                //end trex
+
+            }
+            else
+            {
+                PlayerA.Top += 5;
+            }
+
+            //trex
+
+            foreach (Control ctrl in this.Controls)
+            {
+                if (ctrl is PictureBox && (string)ctrl.Tag == "obstacle")
+                {
+                    ctrl.Left -= obstacleSpeed;
+                    if (ctrl.Left < -100)
+                    {
+                        ctrl.Left = this.ClientSize.Width + rand.Next(10, 50) + ctrl.Width * 15;
+                        score++;
+                    }
+                    if (PlayerA.Bounds.IntersectsWith(ctrl.Bounds))
+                    {
+                        gameTimer.Stop();
+                        PlayerA.Image = Properties.Resources.GameOver;
+                        lblScore.Text += " Press R to restart the game!";
+                        isGameOver = true;
+                    }
+                }
+            }
+            if (score >= 5)
+            {
+                obstacleSpeed = 15;
+            }
+
+        }
+
+        //trex
+        private void GameReset()
+        {
+            force = 12;
+            jumpSpeed = 0;
+            jump = false;
+            score = 0;
+            obstacleSpeed = 10;
+            lblScore.Text = "Score: " + score;
+            PlayerA.Image = Properties.Resources.PlayerA;
+            isGameOver = false;
+            PlayerA.Top = 472;
+
+            foreach (Control ctrl in this.Controls)
+            {
+                if (ctrl is PictureBox && (string)ctrl.Tag == "obstacle")
+                {
+                    position = this.ClientSize.Width + rand.Next(10, 100) + (ctrl.Width * 10);
+                    ctrl.Left = position;
+                }
+            }
+            gameTimer.Start();
+        }
+
+    }
+}
